f1c8a891a5a893beb2a887325586bb3274c4b498
Unknown <diegomugnae@gmail.com>
2018-08-15 22:02:44 -0300

    Feat: Dar match em duas cartas
    Aqui foi adicionada a função checaPar() onde a principio é realizado a mudança de classes e checagem basica de match.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ *   - add each card"s HTML to the page
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -28,11 +28,11 @@ function shuffle(array) {
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - display the card"s symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card"s symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
@@ -42,21 +42,50 @@ function shuffle(array) {
 
  /* função para desvirar as cartas*/
 let deck,carta,baralho;
-
-$('li').click(function() {
-    let classeCarta = $(this).attr('class');
-    if (classeCarta == 'card') {
-      $(this).toggleClass('open show');
-    }else if (classeCarta == 'card open show'){
-      alert('Carta já esta virada, selecione outra!');
+$("li").click(function() {
+    let classeCarta = $(this).attr("class");
+    if (classeCarta == "card") {
+      $(this).toggleClass("open show");
+      checaPar();
+    }else if (classeCarta == "card open show"){
+      alert("Carta já esta virada, selecione outra!");
     } else {
-      alert('Carta ja tem seu par!');
+      alert("Carta ja tem seu par!");
     }
 
 });
 
 
+/**
+ * funçao de checagem
+ * aqui são mudadas as classes caso sucesso
+ */
+function checaPar(){
+  let pardecartas;
+  pardecartas = $(".card.open.show");
+  if (pardecartas.length < 2) {
+      return;
+  }else if ($(pardecartas[0]).children().className == $(pardecartas[1]).children().className){ //needs a fix
+       pardecartas.each(function(index){
+        $(pardecartas[index]).addClass("match"); 
+        $(pardecartas[index]).removeClass("open show");
+        console.log("its a match")
+        return;
+        })
+    }else {     
+      pardecartas.each(function(index){
+        $(pardecartas[index]).removeClass("open show");
+      })
+    }
+    
+    
+  }
+
+
+
 
 });
 /* fim do corpo jQuery */
+
+
 
